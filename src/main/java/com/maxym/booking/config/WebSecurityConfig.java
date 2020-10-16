a2fdb3ec5d29a8b59ac4a348800076d303955589
Maxym <koval.maxim@unicyb.kiev.ua>
2020-10-16 15:58:05 +0300

    Sessions support added

--- src/main/java/com/maxym/booking/config/WebSecurityConfig.java
+++ src/main/java/com/maxym/booking/config/WebSecurityConfig.java
@@ -35,6 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .loginPage("/login")
                     .permitAll()
                 .and()
+                    .rememberMe()
+                .and()
                     .logout()
                     .logoutSuccessUrl("/")
                     .permitAll();
